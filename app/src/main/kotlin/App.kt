7626e542089dcb6532ec02f01b05f2b32d484c5e
Ian Chen <ianchen7777@gmail.com>
2025-02-03 23:11:41 +1100

    Initial commit

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,11 @@
+package com.ianxc.app
+
+import com.ianxc.utils.Printer
+
+// This is the main entry point of the application.
+// It uses the `Printer` class from the `:utils` subproject.
+fun main() {
+    val message = "Hello JetBrains!"
+    val printer = Printer(message)
+    printer.printMessage()
+}
