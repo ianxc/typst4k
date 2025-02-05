6bf5549e9cd35c3cfb03677c864c5a5dfae2d806
Ian Chen <ianchen7777@gmail.com>
2025-02-05 23:17:36 +1100

    Format

--- processWriter/src/test/kotlin/com/ianxc/typst4k/processwriter/ResourceExt.kt
+++ processWriter/src/test/kotlin/com/ianxc/typst4k/processwriter/ResourceExt.kt
@@ -3,6 +3,6 @@ package com.ianxc.typst4k.processwriter
 import java.nio.file.Path
 import kotlin.io.path.toPath
 
-inline fun <reified T: Any> T.resourceOf(location: String): Path? {
+inline fun <reified T : Any> T.resourceOf(location: String): Path? {
     return this.javaClass.getResource(location)?.toURI()?.toPath()
 }
